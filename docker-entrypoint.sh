b9fd2c1da4ce891ab00f02eaa50968fe3b87da01
Anubhav Mishra <anubhavmishra@me.com>
2017-05-08 00:35:08 -0700

    adding authentication for slaves and sentinels

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,24 +11,27 @@ if [ -z "$SENTINEL" ]; then
 	echo "rename-command FLUSHDB \"\"" >> /etc/redis.conf
 	echo "rename-command FLUSHALL \"\"" >> /etc/redis.conf
 	echo "rename-command SHUTDOWN \"\"" >> /etc/redis.conf
-	echo "rename-command SLAVEOF \"\"" >> /etc/redis.conf
 	echo "rename-command DEBUG \"\"" >> /etc/redis.conf
 	echo "rename-command OBJECT \"\"" >> /etc/redis.conf
 	echo "rename-command MOVE \"\"" >> /etc/redis.conf
-fi
-if [ ! -z "$REDIS_PASSWORD" ]; then
-	echo "requirepass $REDIS_PASSWORD" >> /etc/redis.conf
+	if [ ! -z "$REDIS_PASSWORD" ]; then
+		echo "requirepass $REDIS_PASSWORD" >> /etc/redis.conf
+	fi
 fi
 echo "databases 10000" >> /etc/redis.conf
 
 set -- $(which redis-server) /etc/redis.conf
 
 if [ -v REPLICA ] && [ ! -v SENTINEL ]; then
+	echo "masterauth $REDIS_PASSWORD" >> /etc/redis.conf
   set -- $@ --slaveof $(redis-cli -h redis-sentinel -p 26379 --raw sentinel get-master-addr-by-name primary | sed -n 1p) 6379
 fi
 
 if [ -v SENTINEL ]; then
 	echo "sentinel monitor primary $SENTINEL 6379 2" >> /etc/redis.conf
+	if [ ! -z "$REDIS_PASSWORD" ]; then
+		echo "sentinel auth-pass primary $REDIS_PASSWORD" >> /etc/redis.conf
+	fi
 	echo "sentinel down-after-milliseconds primary 5000" >> /etc/redis.conf
 	echo "sentinel failover-timeout primary 10000" >> /etc/redis.conf
 	echo "sentinel parallel-syncs primary 1" >> /etc/redis.conf
